d21bb14598abc42da04092ef7ee04120f511858d
juliaapolonio <apoloniojulia@gmail.com>
2024-03-13 11:07:40 -0300

    Working script

--- run_gcta_gsmr.sh
+++ run_gcta_gsmr.sh
@@ -1,7 +1,7 @@
 #f.Run GSMR for eQTL
 ./gcta-1.94.1-linux-kernel-3-x86_64/gcta-1.94.1   \
---mbfile /data/home/julia.amorim/scripts/data/references/plink_bfile/gsmr_ref_data.txt   \
---gsmr-file /data/home/julia.amorim/scripts/data/references/splitted_exposure_eqtlgen/2.txt /data/home/julia.amorim/scripts/data/references/gsmr_outcome_broad_DEP.txt   \
+--mbfile /storages/acari/julia.amorim/references/plink_bfile/gsmr_ref_data.txt   \
+--gsmr-file /storages/acari/julia.amorim/qtls/pqtl/format/split/1.txt data/gsmr_outcome_broad_DEP.txt   \
 --gsmr-direction 0   \
 --gsmr-snp-min 1   \
 --diff-freq 0.5   \
@@ -9,4 +9,4 @@
 --clump-r2 0.05   \
 --heidi-thresh 0.01   \
 --effect-plot   \
---out /data/home/julia.amorim/scripts/data/outputs/gsmr/gsmr_eQTLGen_result_2
+--out data/gsmr_hillary_result_1
